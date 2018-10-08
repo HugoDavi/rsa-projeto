d403e9ca7b5ac4893c1abd7a86766fbb11b5ce05
Wykthor <wykthor.g@wgvale.com>
2018-10-08 20:18:57 -0300

    Configurações para as requisições do trabalho adicionadas.

--- rsa.py
+++ rsa.py
@@ -37,68 +37,65 @@ def multiplicativeInverse(e, n):
         return n + x
     return x
 #math functions#
+# trabalho functions
+def asciiToAmorim(char):
+	if(char == " "):
+		 return 26
+	elif(ord(char.upper())>=65 and ord(char.upper()) < 65+26):
+		return ord(char.upper())-65
+	else:
+		return 26
 
+def amorimToAscii(numb):
+	if(numb == 26):
+		return " "
+	else:
+		return chr(numb+65)
+# trabalho functions
 # RSA functions
 
-
-def generateKeyManual(p, q):
-    if p == q:
-        print("Valores não válidos, devem ser diferentes.")
-    elif p*q <= 256:
-        print("Valores não válidos, p*q deve ser maior que 256.")
-    elif(not isPrime(p) or not isPrime(q)):
-        print("Valores não válidos, p e q devem ser primos.")
-    else:
-        n = p*q
-        phi = (p-1)*(q-1)
-        while True:
-            e = random.randint(3, phi-1)
-            if fractions.gcd(e, phi) == 1:
-                break
-        d = multiplicativeInverse(e, phi)
-        return (n, e, d)
-
-
-def generateKey(bits):
-    p = generateRandomPrime(int(bits / 2))
-    q = generateRandomPrime(int(bits / 2))
-    # Ensure q != p, though for large values of bits this is
-    # statistically very unlikely
-    while q == p and q*p < 256:
-        q = generateRandomPrime(int(bits / 2))
-    n = p * q
-    phi = (p - 1) * (q - 1)
-    # Here we pick a random e, but a fixed value for e can also be used.
-    while True:
-        e = random.randint(3, phi - 1)
-        if fractions.gcd(e, phi) == 1:
-            break
-    # encrypt((e,n),text)
-    # decrypt((p,q,e),text)
-    privateKey = multiplicativeInverse(e, phi)
-    publicKey = e
-    return (publicKey, privateKey, n)
-
-# d
-
+def generatePrivateKey(p,q):
+	phi = (p-1)*(q-1)
+	while True:
+		e = random.randint(3, phi-1)
+		if fractions.gcd(e,phi) == 1:
+			break
+	return e
+		
+def generatePublicKey(p, q, e):
+	if p == q:
+		print("Valores não válidos, devem ser diferentes.")
+	elif p*q <= 256:
+		print("Valores não válidos, p*q deve ser maior que 256.")
+	elif(not isPrime(p) or not isPrime(q)):
+		print("Valores não válidos, p e q devem ser primos.")
+	elif(fractions.gcd(e,((p-1)*(q-1)))!=1):
+		print("Valores não válidos, 'e' deve ser coprimo a (p-1)*(q-1)")
+	else:
+		n = p*q
+		phi = (p-1)*(q-1)
+		d = multiplicativeInverse(e, phi)
+		return (n, e, d)
 
 def encrypt(publicKey, n, plaintext):
     # Unpack the key into its components
     n = int(n)
     key = int(publicKey)
     # Convert each letter in the plaintext to numbers based on the character using a^b mod m with fast modular exponentiation
-    cipher = [str(pow(ord(char), key, n)) for char in plaintext]
+    cipher = [pow(asciiToAmorim(char), key, n) for char in plaintext]
     # Return the array of bytes
-    return ' '.join(cipher)
+    return cipher
 
 
 def decrypt(privateKey, n, ciphertext):
-    # Unpack the key into its components
-    ciphertext = ciphertext.split()
-    n = int(n)
-    key = int(privateKey)
-    # Generate the plaintext based on the ciphertext and key using a^b mod m with fast modular exponentiation
-    plain = [chr(pow(int(char), key, n)) for char in ciphertext]
-    # Return the array of bytes as a string
-    return ''.join(plain)
+	# Unpack the key into its components
+	#ciphertext = ciphertext.split()
+	n = int(n)
+	key = int(privateKey)
+	# Generate the plaintext based on the ciphertext and key using a^b mod m with fast modular exponentiation
+	plain = [pow(int(char), key, n) for char in ciphertext]
+	print(plain)
+	plain = [amorimToAscii(char) for char in plain]
+	# Return the array of bytes as a string
+	return ''.join(plain)
 #RSA functions#
